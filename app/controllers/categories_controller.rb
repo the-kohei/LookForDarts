04939e79e8587787aa16f4c0714ef298eb304d99
the-kohei <ky19930221ky@gmail.com>
2020-05-12 00:55:24 +0900

    category 一覧表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
-def index
-end
+  def index
+    @categories = Category.all
+  end
 
   def show
     @categories = Category.limit(8).order('id DESC')
