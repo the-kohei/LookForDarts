e0aea1f80f09d09f93bcdb5e9b2dedb8fc2e0a1f
the-kohei <ky19930221ky@gmail.com>
2020-06-01 23:50:06 +0900

    comment 修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       get 'category_posts'
       get 'maker_posts'
     end
-    resources :comments, only: :create
+    resources :comments, only: [:create]
     collection do
       get 'search'
     end
